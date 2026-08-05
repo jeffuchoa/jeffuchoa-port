0ab61032d1a7279aaf1b2b6d55eab817f2d9f6fa
jeffuchoa <jefferson.uchoa86@gmail.com>
2026-08-05 14:49:58 -0300

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import { Fragment, useEffect } from "react";
-import PreLoader from "../src/layout/PreLoader";
 import { aTagClick, kura_tm_my_load, wowJsAnimation } from "../src/utilits";
 import "../styles/globalstyle.css";
 
@@ -12,7 +11,6 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <Fragment>
-    <PreLoader />
     <Component {...pageProps} />
     </Fragment>
   );
