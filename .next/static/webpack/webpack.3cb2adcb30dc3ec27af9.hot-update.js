fae86bdcba625850a4bf41532ee78874da7af2f6
jeffuchoa <jefferson.uchoa86@gmail.com>
2026-08-05 17:40:55 -0300

    update

--- .next/static/webpack/webpack.3cb2adcb30dc3ec27af9.hot-update.js
+++ .next/static/webpack/webpack.3cb2adcb30dc3ec27af9.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "dea9137e7bb9f0098426"; }
+/******/ 	__webpack_require__.h = function() { return "364fafb5474649535acd"; }
 /******/ }();
 /******/ 
 /******/ }
