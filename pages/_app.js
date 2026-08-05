f917c6b26aba3addbb4051cf818b6268a6592085
jeffuchoa <jefferson.uchoa86@gmail.com>
2026-08-05 14:13:10 -0300

    update

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,18 @@ import { Fragment, useEffect } from "react";
 import PreLoader from "../src/layout/PreLoader";
 import { aTagClick, kura_tm_my_load, wowJsAnimation } from "../src/utilits";
 import "../styles/globalstyle.css";
+
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     kura_tm_my_load();
     wowJsAnimation();
     aTagClick();
-  });
+  }, []); // <--- ADICIONE ESTES COLCHETES AQUI
+
   return (
     <Fragment>
-      <PreLoader />
-      <Component {...pageProps} />
+    <PreLoader />
+    <Component {...pageProps} />
     </Fragment>
   );
 }
