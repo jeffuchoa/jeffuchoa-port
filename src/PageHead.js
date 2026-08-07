51b6bf0944396e0057988c91b3ee368f270c05cb
jeffuchoa <jefferson.uchoa86@gmail.com>
2026-08-07 12:28:10 -0300

    update-header

--- src/PageHead.js
+++ src/PageHead.js
@@ -1,7 +1,16 @@
 import Head from "next/head";
+
 const PageHead = ({ page }) => {
   return (
     <Head>
+      <meta charset="UTF-8" />
+      <meta property="og:image" content="/img/logo/banner.webp" />
+      <meta property="og:title" content="Jeff Uchoa" />
+      <meta property="og:description" content="Site Profissional" />
+      <meta property="og:url" content="https://jeffuchoa.com.br/" />
+      <meta property="og:type" content="website" />
+      <link rel="icon" type="image/webp" href="/img/logo/icon.webp" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Jeff Uchoa | {page}</title>
     </Head>
   );
